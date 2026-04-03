dad93909d073b40427aff631e38a8123f8057611
jvtoti <48146417+jvtoti@users.noreply.github.com>
2026-04-03 20:18:00 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'main.js'
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true
+  }
+})
